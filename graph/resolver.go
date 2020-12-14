c1535a7f7df5cc4b19dcb3167a4234c9c30fb53b
Bassel Al Araaj <info@basselalaraaj.com>
2020-12-15 00:35:38 +0100

    fix: validate schema before trying to save it in redis

--- graph/resolver.go
+++ graph/resolver.go
@@ -2,16 +2,19 @@ package graph
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/basselalaraaj/graphql-schema-registry/graph/generated"
 	"github.com/basselalaraaj/graphql-schema-registry/graph/model"
+	"github.com/go-redis/redis/v8"
+	"github.com/vektah/gqlparser"
+	"github.com/vektah/gqlparser/ast"
 )
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
+var ctx = context.Background()
 
+// It serves as dependency injection for your app, add any dependencies you require here.
 type Resolver struct{}
 
 // Mutation returns generated.MutationResolver implementation.
@@ -26,8 +29,42 @@ func (r *Resolver) Query() generated.QueryResolver {
 
 type mutationResolver struct{ *Resolver }
 
+type schemaRegistry struct {
+	ServiceName string
+	ServiceURL  string
+	TypeDefs    string
+}
+
 func (r *mutationResolver) PushSchema(ctx context.Context, schemaInput model.SchemaInput) (bool, error) {
-	fmt.Println(schemaInput)
+	if _, err := gqlparser.LoadSchema(&ast.Source{Name: schemaInput.ServiceName, Input: schemaInput.TypeDefs, BuiltIn: false}); err != nil {
+		fmt.Println(err)
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+
+	key := schemaInput.ServiceName
+
+	schemaRegistry := &schemaRegistry{
+		ServiceName: schemaInput.ServiceName,
+		ServiceURL:  schemaInput.ServiceURL,
+		TypeDefs:    schemaInput.TypeDefs,
+	}
+
+	value, _ := json.Marshal(schemaRegistry)
+
+	err := rdb.Set(ctx, key, value, 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	val, err := rdb.Get(ctx, key).Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(val)
 	return true, nil
 }
 
